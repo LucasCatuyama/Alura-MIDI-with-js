1e5093c4ae5abf492e25a560a466c63e1cb898d9
Lucas <67424170+LucasCatuyama@users.noreply.github.com>
2024-01-24 08:54:43 +0000

    Added the functions

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-function tocaSomPom () {
-    
+function tocaSom(seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
 }
 
-document.querySelector('.tecla_pom');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('#som_tecla_pom').play();
+let contador = 0;
 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
 
